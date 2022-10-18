13d8f7fafd52c0733f74af3fc537b967530df98d
ChrisLee <chirsleecn@outlook.com>
2022-10-18 19:56:21 +0800

    Refactor : Delete useless main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <moe/decorator.hpp>
-
-
-int main() {
-	std::cout << moe::set_decorator(moe::Decorator::c_blue) << "Hello, World!"
-	          << std::endl;
-	return 0;
-}
