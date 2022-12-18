6df19b2cc71ffe33e74ba3e3c42233ae3d31a887
ChrisLee <chirsleecn@outlook.com>
2022-12-18 23:08:49 +0800

    Demo : Add spg hw demo.

---
+++ demo/demo_spg_hw/run_demo_spg.sh
@@ -0,0 +1,44 @@
+# Run at root of project.
+root_judgement=$(sed -n '2p' CMakeLists.txt)
+if [ "${root_judgement}" != "project(Compiler_Projects)" ]; then
+  echo "Judgement finds that second line of ${PWD} is not \"project(Compiler_Projects)\", check if you are running this script on the project root."
+  exit 1
+fi
+
+cmake_folder=cmake-build-debug
+# cmake
+mkdir -p "${cmake_folder}"
+cmake -B "${cmake_folder}" -G "Unix Makefiles"
+# build lialex and liaspg
+cd ${cmake_folder} && make LiaLEX && cd ..
+cd ${cmake_folder} && make LiaSPG && cd ..
+
+lialex_exe=${cmake_folder}/LiaLEX/LiaLEX
+liaspg_exe=${cmake_folder}/LiaSPG/LiaSPG
+
+this_demo_path=demo/demo_spg_hw
+
+lialex_file=${this_demo_path}/SLR.lialex
+#lialex_file=${this_demo_path}/LL_with_literal.lialex
+liaspg_file=${this_demo_path}/SLR.liagra
+#liaspg_file=${this_demo_path}/LL_with_literal.liagra
+target_lexer_info_hpp=${this_demo_path}/src/ll_lexer_info.hpp
+target_parser_info_hpp=${this_demo_path}/src/ll_parser_info.hpp
+lexer_info_name=ll_lexer_info
+parser_info_name=ll_parser_info
+
+this_demo_exe=${cmake_folder}/demo/demo_spg_hw/demo_spg_hw
+this_demo_src=${this_demo_path}/src.LL
+this_demo_dst=${this_demo_path}/ast.dot
+
+# generate lexer and parser info
+${lialex_exe} --lex ${lialex_file} -o ${target_lexer_info_hpp} --name ${lexer_info_name}
+${liaspg_exe} --lex ${lialex_file} --gra ${liaspg_file} --name ${parser_info_name} \
+  --dest ${target_parser_info_hpp}
+
+# make demo
+cd ${cmake_folder} && make demo_spg_hw && cd ..
+
+# run this demo. stdin is source file. stdout is dest file(a dot file represents ast).
+${this_demo_exe} <${this_demo_src} >${this_demo_dst}
+echo $?
